ae4c596f9e3930d06c168040dfd25b42bc47439b
peabody28 <onmmanmn@gmail.com>
2020-06-21 12:53:49 +0300

    Все на composer

--- db.php
+++ db.php
@@ -1,5 +1,8 @@
 <?php
-require "rb.php";
+require_once "vendor/autoload.php";
+
+class_alias('\RedBeanPHP\R', '\R');
+
 R::setup( 'mysql:host=localhost;dbname=users',
     'root', '1234' );
 R::addDatabase( 'posts', 'mysql:host=localhost;dbname=posts', 'root', '1234');
